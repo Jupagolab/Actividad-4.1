93511e8d69fd773c00cf39b31ca46f26db07edc9
MariaSuarezAraujo <sergiosimancas19@gmail.com>
2023-11-24 01:00:42 -0400

    1er Commit
    Se hizo varios import y se creo el código principal para el funcionamiento visibilidad  de la interfaz del diccionario. Ademas se creo la carpeta componentes y se creio varios archivos .js en dicha carpeta.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
+import './styles/modo-oscuro.css';
+import './styles/resultado.css';
+import './styles/fuentes.css';
+import Boton from './componentes/Boton';
+import Buscar from './componentes/Buscar';
+import TemaSistema from './componentes/TemaSistema'
 
 function App() {
   
   const body = document.body;
 
+  const Tema = () => {
+    body.classList.toggle('Darkmode');
+  }  
+
   return (
     <div className="App">
+      <TemaSistema />
       <header>
         <div className="titulo">
           <h1>Diccionario en Inglés</h1>
         </div>
         <div className="modoOscuro">
-          
+          <Boton 
+            tipoBoton={ Tema }
+          />
         </div>
       </header>
       <main>
-
+        <Buscar />
       </main>
 
 
