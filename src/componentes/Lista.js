c81e1db545a00de887dfb3ec44c6165b5cd46d9f
Jupagolab <jupagolab@gmail.com>
2023-11-24 19:23:57 -0400

    Update Lista.js

---
+++ src/componentes/Lista.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Lista = () => {
+
+  return (
+    <>
+      <select id="Fuentes" >
+        <option value={"fuente1"}>Open Sans</option>
+        <option value={"fuente2"}>Roboto</option>
+        <option value={"fuente3"}>Lato</option>
+        <option value={"fuente4"}>Montserrat</option>
+        <option value={"fuente5"}>Poppins</option>
+        <option value={"fuente6"}>Source Sans Pro</option>
+      </select>
+    </>
+  )
+}
+
+export default Lista;
