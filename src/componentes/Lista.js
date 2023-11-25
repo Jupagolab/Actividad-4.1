ff695f112c4674bbb9068566ae6c21cf56502914
LuisMartinez211 <martinezstanislaolufe@uvm.edu.ve>
2023-11-24 20:46:42 -0400

    8vo commit
    Se agrega la parte de Lista.js a componentes

--- src/componentes/Lista.js
+++ src/componentes/Lista.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Lista = () => {
 
+  // Nuevo estado para el tipo de fuente seleccionado
+  const [selectedFont, setSelectedFont] = useState('Open Sans');
+
+  // Función para cambiar la fuente
+  const handleChangeFont = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedFont(selectedValue);
+    // Aplica el estilo al body o a cualquier otro elemento que desees
+    document.body.classList.add(selectedValue);
+  };
+
   return (
     <>
-      <select id="Fuentes" >
+      <select id="Fuentes" value={selectedFont} onChange={handleChangeFont}>
         <option value={"fuente1"}>Open Sans</option>
         <option value={"fuente2"}>Roboto</option>
         <option value={"fuente3"}>Lato</option>
